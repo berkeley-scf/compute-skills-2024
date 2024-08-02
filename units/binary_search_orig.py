bf7882f7688ec1fab188d5318073b4ba33e8bb31
Christopher Paciorek <paciorek@stat.berkeley.edu>
2024-08-02 16:09:17 -0700

    Add original code file.

---
+++ units/binary_search_orig.py
@@ -0,0 +1,36 @@
+import math
+
+
+def search(lst, T):
+    L = 0
+    R = len(lst) - 1
+    while L < R:
+        m = math.floor((L + R) / 2)
+        L, R = update_interval(lst, m, T, L, R)
+    if lst[L] == T:
+        return L
+    return -1
+
+
+def update_interval(lst, m, T, L,       R):
+    if lst[m] <= T:
+     L = m + 1
+    else:
+         R = m - 1
+    return L, R
+
+
+def search_fix(lst, T):
+    L = 0
+    R = len(lst) - 1
+    while L < R:
+        m = math.floor((L + R) / 2)
+        if lst[L] == T:
+            return L
+        L, R = update_interval(lst, m, T, L, R)
+    if lst[L] == T:
+        return L
+    return -1
+
+def testfun(x =   0):
+    return x
